ead8b28f1dc16ca96e97fb3a33d97dfb76b8193e
aswinss18 <aswinartography18@gmail.com>
2024-08-04 06:50:10 +0530

    Displaying is progress is created

---
+++ src/Components/Progress.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Progress({
+  index,
+  numQuestions,
+  maxPossiblePoints,
+  points,
+  answer,
+}) {
+  return (
+    <header className="progress">
+      <progress
+        max={numQuestions}
+        value={index + Number(answer !== null)}
+      ></progress>
+      <p>
+        Question <strong>{index + 1}</strong> /{numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+}
